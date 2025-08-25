3094c1fde46ab81f6a4e2e826791e184a58d1f04
lucca170 <gugalucasilveira@gmail.com>
2025-08-25 08:04:09 -0300

    Inicio Frontend

--- Agenda 3BIM/backend/core/settings.py
+++ Agenda 3BIM/backend/core/settings.py
@@ -9,12 +9,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
-   'django.contrib.admin',
+    'jazzmin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
    
     'rest_framework',
     'corsheaders',  
